f25232bc6e73964d638cb568f89a0f99ed4346a5
Jash Savsani <savsanijash8@gmail.com>
2025-02-25 18:50:21 +0530

    Add files via upload

--- Pro1.java
+++ Pro1.java
@@ -1,19 +1,20 @@
-//WAP to find a sum of even number into 1D array.
-import java.util.Scanner;
 class Pro1{
 	public static void main(String[] args) {
-		Scanner sc  = new Scanner(System.in);
-		System.out.print("Enter Number of Elements in Array:");
-		int n= sc.nextInt();
-		int a[] = new int[n];
-		int sum=0;
-		for(int i=0; i<n;i++){
-			System.out.print("Enter a["+i+"]:");
-			a[i] = sc.nextInt();
-			if(a[i]%2==0){//evenNum
-				sum+=a[i];
-			}
-		}
-		System.out.print("Sum of Even Num in 1D array is: "+sum);
+		String s1 = "Hello";
+		String s2 = "Hello";
+		String s3 = new String("Hello");
+		String s4 = new String("Hello");
+		String s5 = s4;
+		String s6 = s1;
+		System.out.println(s1.equals(s4));
+		System.out.println(s6==s1);
+		System.out.println(s2==s3);
+		System.out.println(s1==s2);
+		s1 = "Jash";
+		System.out.println(s2);
+		System.out.println(s3==s4);
+		System.out.println(s5==s4);
+		
+
 	}
 }
